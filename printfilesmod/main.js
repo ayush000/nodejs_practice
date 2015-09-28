e3879509d7973c93650e9c9b9bc4761fe1c7b333
Ayush Sachdeva <ayush.sachdeva@mail.jugnoo.in>
2015-09-28 17:48:25 +0530

    Initialized modules

---
+++ printfilesmod/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by socomo14 on 9/28/15.
+ */
+var readnfilter=require('./readnfilter');
+//var callback=function(err,data)
+//{
+//    if(err)
+//    {
+//        return ca
+//    }
+//};
+readnfilter(callback)
---
+++ printfilesmod/readnfilter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by socomo14 on 9/28/15.
+ */
+module.exports=function(dirname,ext,callback)
+{
+    fs.readdir(dirname,callback);
+
+};
